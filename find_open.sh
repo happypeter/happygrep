4f98a8549390943e9a554735f329f7264ed5a742
Peter Wang <happypeter1983@gmail.com>
2011-02-18 17:27:07 +0800

    add a bot readability

--- find_open.sh
+++ find_open.sh
@@ -8,8 +8,7 @@
 if [ $# -eq 0 ] 
 then
     echo """
-    usage:
-        xxx string
+    Usage: xxx PATTERN 
     """
     exit 1
 fi
@@ -32,7 +31,7 @@ if [ -z "$ttt_content" ]
 then
     echo  no match
     rm ttt
-    exit ## ttt is empty, so no match
+    exit ## ttt is empty, so no match, quit
 fi
 
 tac ttt > reversed ## reverse lines in the file
@@ -67,13 +66,13 @@ rm reversed
 declare -a thefilename
 declare -a linenumber
 
-foo=0
+count=0
 while read line; do 
-    echo -ne "\033[1m NO.$(( ++foo )):\033[0m"
-    thefilename[$foo]=`echo $line|awk -F":" '{print $1}'`
-    linenumber[$foo]=`echo $line|awk -F":" '{print $2}'`
+    echo -ne "\033[1m NO.$(( ++count )):\033[0m"
+    thefilename[$count]=`echo $line|awk -F":" '{print $1}'`
+    linenumber[$count]=`echo $line|awk -F":" '{print $2}'`
     linecontent=`echo $line|awk -F":" '{print $3}'` # FIXME:it is possible to have "http://xxx" as content
-    echo ${thefilename[$foo]}
+    echo ${thefilename[$count]}
     echo '    ' $linecontent
 done < output_file
 rm output_file
@@ -90,23 +89,3 @@ echo -n " Now which one do you want to open? [1,2,3...]  "
 read nnn
 
 vim +${linenumber[$nnn]} ${thefilename[$nnn]}
-#
-#case $nnn in
-#1)
-#vim +${linenumber[1]} ${thefilename[1]};;
-#2)
-#vim +${linenumber[2]} ${thefilename[2]};;
-#3)
-#vim +${linenumber[3]} ${thefilename[3]};;
-#4)
-#vim +${linenumber[4]} ${thefilename[4]};;
-#5)
-#vim +${linenumber[5]} ${thefilename[5]};;
-#6)
-#vim +${linenumber[6]} ${thefilename[6]};;
-#7)
-#vim +${linenumber[7]} ${thefilename[7]};;
-#8)
-#vim +${linenumber[8]} ${thefilename[8]};;
-#esac
-##
