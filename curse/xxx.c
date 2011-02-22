68f6113fd454c8edaf1d1ef0dd4ecd1cf197d87f
Peter Wang <happypeter1983@gmail.com>
2011-02-22 11:38:29 +0800

    clean codebase, nothing added

--- curse/xxx.c
+++ curse/xxx.c
@@ -1,8 +1,3 @@
-/*
- *
- *
- */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -14,46 +9,9 @@
  * Init and quit
  */
 
-static void quit(int sig)
-{
-	endwin();
-    printf("printf quit\n");
-
-	/* do your non-curses wrapup here */
-
-	exit(0);
-}
-
-static void
-init_colors(void)
-{
-	start_color();
-
-	init_pair(COLOR_BLACK,	 COLOR_BLACK,	COLOR_BLACK);
-	init_pair(COLOR_GREEN,	 COLOR_GREEN,	COLOR_BLACK);
-	init_pair(COLOR_RED,	 COLOR_RED,	COLOR_BLACK);
-	init_pair(COLOR_CYAN,	 COLOR_CYAN,	COLOR_BLACK);
-	init_pair(COLOR_WHITE,	 COLOR_WHITE,	COLOR_BLACK);
-	init_pair(COLOR_MAGENTA, COLOR_MAGENTA,	COLOR_BLACK);
-	init_pair(COLOR_BLUE,	 COLOR_BLUE,	COLOR_BLACK);
-	init_pair(COLOR_YELLOW,	 COLOR_YELLOW,	COLOR_BLACK);
-}
-
-static void
-init(void)
-{
-    (void) signal(SIGINT, quit);      /* arrange interrupts to terminate */
-    // when you <Ctr-c>, SIGINT is sent to this process, and quit() is called
-
-	(void) initscr();      /* initialize the curses library */
-	keypad(stdscr, TRUE);  /* enable keyboard mapping */
-	(void) nonl();         /* tell curses not to do NL->CR/NL on output */
-	(void) cbreak();       /* take input chars one at a time, no wait for \n */
-	(void) noecho();       /* don't echo input */
-
-	if (has_colors())
-		init_colors();
-}
+static void init_colors(void);
+static void quit(int sig);
+static void init(void);
 
 /*
  * Main
@@ -100,31 +58,78 @@ main(int argc, char *argv[])
 	attrset(A_NORMAL);
 /*        scrollok();*/
 
-	for (;;) {
+	for (;;) 
+    {
 		int c = getch();     /* refresh, accept single keystroke of input */
 
 		/* Process the command keystroke */
-		switch (c) {
-		case 'q':
-			quit(0);
-			return 0;
+		switch (c) 
+        {
+            case 'q':
+                quit(0);
+                return 0;
 
-		case 's':
-			addstr("Shelling out...");
-			def_prog_mode();           /* save current tty modes */
-			endwin();                  /* restore original tty modes */
-			system("sh");              /* run shell */
-			addstr("returned.\n");     /* prepare return message */
-			reset_prog_mode();
-			//refresh();                 /* restore save modes, repaint screen */
-			break;
+            case 's':
+                addstr("Shelling out...");
+                def_prog_mode();           /* save current tty modes */
+                endwin();                  /* restore original tty modes */
+                system("sh");              /* run shell */
+                addstr("returned.\n");     /* prepare return message */
+                reset_prog_mode();
+                //refresh();                 /* restore save modes, repaint screen */
+                break;
 
-		default:
-			if (isprint(c) || isspace(c))
-				addch(c);
+            default:
+                if (isprint(c) || isspace(c))
+                    addch(c);
 		}
 
 	}
 
 	quit(0);
+}
+
+static void 
+quit(int sig)
+{
+	endwin();
+    printf("printf quit\n");
+
+	/* do your non-curses wrapup here */
+
+	exit(0);
+}
+
+static void
+init_colors(void)
+{
+	start_color();
+
+	init_pair(COLOR_BLACK,	 COLOR_BLACK,	COLOR_BLACK);
+	init_pair(COLOR_GREEN,	 COLOR_GREEN,	COLOR_BLACK);
+	init_pair(COLOR_RED,	 COLOR_RED,	COLOR_BLACK);
+	init_pair(COLOR_CYAN,	 COLOR_CYAN,	COLOR_BLACK);
+	init_pair(COLOR_WHITE,	 COLOR_WHITE,	COLOR_BLACK);
+	init_pair(COLOR_MAGENTA, COLOR_MAGENTA,	COLOR_BLACK);
+	init_pair(COLOR_BLUE,	 COLOR_BLUE,	COLOR_BLACK);
+	init_pair(COLOR_YELLOW,	 COLOR_YELLOW,	COLOR_BLACK);
+}
+
+static void
+init(void)
+{
+    signal(SIGINT, quit);      /* arrange interrupts to terminate */
+    // when you <Ctr-c>, SIGINT is sent to this process, and quit() is called
+
+	initscr();      /* initialize the curses library */
+	keypad(stdscr, TRUE);  /* enable keyboard mapping */
+	nonl();         /* tell curses not to do NL->CR/NL on output */
+	cbreak();       /* take input chars one at a time, no wait for \n */
+	noecho();       /* don't echo input */
+    scrollok(stdscr, TRUE);
+
+	if (has_colors())
+    {
+		init_colors();
+    }
 }
