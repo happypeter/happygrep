67a5538843ddcc267311f6283cf76da11fe2717e
Peter Wang <happypeter1983@gmail.com>
2012-06-23 11:25:45 +0800

    less mess, more info

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,24 +1,6 @@
 (function($) {
 $(document).ready(function(){
 
-  // putting lines by the pre blocks
-  $("pre").each(function(){
-    var pre = $(this).text().split("\n");
-    var lines = new Array(pre.length+1);
-    for(var i = 0; i < pre.length; i++) {
-      var wrap = Math.floor(pre[i].split("").length / 70)
-      if (pre[i]==""&&i==pre.length-1) {
-        lines.splice(i, 1);
-      } else {
-        lines[i] = i+1;
-        for(var j = 0; j < wrap; j++) {
-          lines[i] += "\n";
-        }
-      }
-    }
-    $(this).before("<pre class='lines'>" + lines.join("\n") + "</pre>");
-  });
-
   var headings = [];
 
   var collectHeaders = function(){
